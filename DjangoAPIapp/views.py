7b1f65cfbf6831c34f11dcd74818c4b8c70cf29d
Akshayaa23 <105343388+Akshayaa23@users.noreply.github.com>
2022-05-31 12:33:19 +0530

    completed

---
+++ DjangoAPIapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from rest_framework import generics
+from DjangoAPIapp.models import Category,Book,Product
+from .serializers import CategorySerializer,BookSerializer,ProductSerializer
+
+
+class ListCategory(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+
+class DetailCategory(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    
+class ListBook(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+
+class DetailBook(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+   
+
+class ListProduct(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class DetailProduct(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+   
+
+
+
+
+
